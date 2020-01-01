fc5e68a27a9c0cecd44de8db658c795decc9b3dd
sloothes <sloothes.com@gmail.com>
2020-01-01 17:29:31 +0200

    Update

--- js/app.js
+++ js/app.js
@@ -46,10 +46,6 @@ var APP = {
 			//	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 			}
-			
-		//	renderer.domElement.style.backgroundSize = "cover";
-		//	renderer.domElement.style.backgroundRepeat = "no-repeat";
-		//	renderer.domElement.style.backgroundImage = "url(https://i.postimg.cc/rmcWddkS/leg-PNG4817.png)";
 
 			this.dom.appendChild( renderer.domElement );
 			this.setScene( loader.parse( json.scene ) );
